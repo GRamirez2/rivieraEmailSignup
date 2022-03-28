7cdf52eb1209ecaceb3d3200fd3100e249641e59
George Ramirez <george.ramirez2@gmail.com>
2022-03-27 19:53:44 -0500

    updated the form to return html on success

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const html = '<h1>Success</h1>'
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+  console.log('req.body.value', req.body.value)
+  if(req.body.value === 'ken' && req.body.count < 4){
+  res.status(200).json({ collector: html })
+  }
+}  
+
